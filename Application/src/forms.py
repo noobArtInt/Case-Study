9789474f503d07a106308efe6aed9b552e8187e1
Anuj Karn <anujkn05@gmail.com>
2020-06-14 10:26:10 +0530

    Add a bit of bootstrap

--- Application/src/forms.py
+++ Application/src/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired, Length
 from flask_wtf.csrf import CSRFProtect, CSRFError
 
 class LoginForm(FlaskForm):
-	user = StringField("Username", validators=[DataRequired()])
-	password = PasswordField("Password", validators=[DataRequired()])
+	user = StringField("Username", validators=[DataRequired(), Length(min=8)])
+	password = PasswordField("Password", validators=[DataRequired(), Length(min=10)])
 	submit = SubmitField("Login")
+	print(user, password)
 
